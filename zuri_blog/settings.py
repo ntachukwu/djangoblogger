14fe8fdf007b351ea6408dec7468464184dcac79
Th3nn3ss <chuksmcdennis@yahoo.com>
2021-04-25 11:56:57 +0100

    fixed virtual environment issues v2

--- zuri_blog/settings.py
+++ zuri_blog/settings.py
@@ -139,4 +139,6 @@ LOGOUT_REDIRECT_URL = "home"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 django_heroku.settings(locals())
