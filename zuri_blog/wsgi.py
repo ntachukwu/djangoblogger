d6971bffaeebe720c047f13fa89dbed337b7a222
Th3nn3ss <chuksmcdennis@yahoo.com>
2021-04-25 12:35:24 +0100

    fixed wsgi.py whitenoise

--- zuri_blog/wsgi.py
+++ zuri_blog/wsgi.py
@@ -7,7 +7,6 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
-from whitenoise.django import DjangoWhiteNoise
 import os
 
 from django.core.wsgi import get_wsgi_application
@@ -15,4 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'zuri_blog.settings')
 
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
